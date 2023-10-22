f0d1599039b1fa669f0556b9f965521ab6a77727
Bartoshuk Nazar <bartoshuknazar0602@gmail.com>
2023-10-22 09:18:09 +0300

    Доробив показ ігор

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,33 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Net.Http;
 using System.Runtime.CompilerServices;
-using System.Text;
+using System.Runtime.Intrinsics.Arm;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using HtmlAgilityPack;
 
 namespace KYRSOVA
 {
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private bool _gamesParsed; // Флаг, що показує, чи були ігри спарсені
-        private List<Game> _games; // Список ігор
-        private List<Game> _selectedGames; // Список вибраних ігор
-        private List<string> _genres; // Список жанрів
-        private string _selectedGenre; // Обраний жанр
+        private bool _gamesParsed;
+        private List<Game> _games;
+        private List<Game> _selectedGames;
+        private List<string> _genres;
+        private string _selectedGenre;
 
-        public List<Game> Games // Список ігор
+        public List<Game> Games
         {
-            get { return _games; }
+            get => _games;
             set
             {
                 _games = value;
@@ -36,9 +30,9 @@ namespace KYRSOVA
             }
         }
 
-        public List<string> Genres // Список жанрів
+        public List<string> Genres
         {
-            get { return _genres; }
+            get => _genres;
             set
             {
                 _genres = value;
@@ -46,27 +40,52 @@ namespace KYRSOVA
             }
         }
 
-
-
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             _selectedGames = new List<Game>();
             _gamesParsed = false;
-            ParseGamesAsync(); // Викликаємо метод парсингу автоматично
+            ParseGamesAsync();
+        }
 
+        private async Task ParseGamesAsync()
+        {
+            var games = new List<Game>();
+            var urlBase = "https://store.steampowered.com/search/?sort_by=_ASC&supportedlang=ukrainian&category1=1&page=";
+
+            for (int page = 1; page <= 2; page++)
+            {
+                var url = urlBase + page;
+                var web = new HtmlWeb();
+                var doc = await web.LoadFromWebAsync(url);
+                var nodes = doc.DocumentNode.SelectNodes("//div[@id='search_resultsRows']/a");
+
+                foreach (var node in nodes)
+                {
+                    var nameNode = node.SelectSingleNode(".//span[@class='title']");
+                    var priceNode = node.SelectSingleNode(".//div[@class='col search_price_discount_combined responsive_secondrow']");
+                    var imageNode = node.SelectSingleNode(".//img");
+
+                    var game = new Game
+                    {
+                        Name = nameNode?.InnerText?.Trim(),
+                        Price = priceNode?.InnerText?.Trim(),
+                        ImageUrl = imageNode?.GetAttributeValue("src", "") ?? "Empty"
+                    };
+
+                    games.Add(game);
+                }
+            }
+            Games = games;
         }
 
 
 
-
-        private void GamesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) // Вибір ігор
+        private void GamesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selectedItems = GamesListView.SelectedItems;
             _selectedGames.Clear();
-
-            foreach (var item in selectedItems) // Додавання вибраних ігор в список вибраних ігор
+            foreach (var item in GamesListView.SelectedItems)
             {
                 if (item is Game game)
                 {
@@ -77,25 +96,19 @@ namespace KYRSOVA
 
         private void PlaySelectedButton_Click(object sender, RoutedEventArgs e)
         {
-            // Отримуємо обрані елементи зі списку вибраних ігор
             var selectedGames = SelectedGamesListView.SelectedItems.Cast<Game>().ToList();
-
             if (selectedGames.Any())
             {
-                // Виконуємо дії для запуску обраних ігор зі збереженими властивостями ігор.
-                // В даному випадку просто відобразимо повідомлення з назвами ігор, які обрали.
                 var gamesNames = string.Join(", ", selectedGames.Select(game => game.Name));
                 MessageBox.Show($"Starting selected games: {gamesNames}", "Games Launch", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             else
             {
-                // Ви можете виконати інші дії у випадку, якщо не обрано жодної гри
                 MessageBox.Show("Please select games to play.", "Games Selection", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
-
-        private void MoveButton_Click(object sender, RoutedEventArgs e) // Переміщення вибраних ігор в список вибраних ігор
+        private void MoveButton_Click(object sender, RoutedEventArgs e)
         {
             foreach (var game in _selectedGames)
             {
@@ -105,8 +118,8 @@ namespace KYRSOVA
                     SelectedGamesListView.Items.Add(game);
                 }
             }
-
         }
+
         private void SelectedGamesListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var selectedGame = SelectedGamesListView.SelectedItem as Game;
@@ -115,10 +128,10 @@ namespace KYRSOVA
                 MessageBox.Show($"Name: {selectedGame.Name}\nPrice: {selectedGame.Price}");
             }
         }
+
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult result = MessageBox.Show("Are you sure you want to clear your library?", "Delete confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
             if (result == MessageBoxResult.Yes)
             {
                 SelectedGamesListView.Items.Clear();
@@ -134,58 +147,13 @@ namespace KYRSOVA
         {
             RegistrationWindow registrationWindow = new RegistrationWindow();
             registrationWindow.ShowDialog();
-
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
             string searchText = SearchGameTextBox.Text.ToLower();
-
             var matchingGames = Games.Where(game => game.Name.ToLower().Contains(searchText)).ToList();
-
             GamesListView.ItemsSource = matchingGames;
-        }
-
-        private async Task ParseGamesAsync()
-        {
-            var games = new List<Game>();
-            var urlBase = "https://store.steampowered.com/search/?sort_by=_ASC&category1=998&page=";
-
-            for (int page = 1; page <= 5; page++) // Парсити перші 
-            {
-                var url = urlBase + page;
-                var web = new HtmlWeb();
-                var doc = await web.LoadFromWebAsync(url);
-                var nodes = doc.DocumentNode.SelectNodes("//div[@class='responsive_search_name_combined']");
-
-                foreach (var node in nodes) // Парсинг ігор
-                {
-                    var nameNode = node.SelectSingleNode(".//span[@class='title']");
-                    var priceNode = node.SelectSingleNode(".//div[@class='col search_price_discount_combined responsive_secondrow']");
-
-                    if (nameNode != null && priceNode != null)
-                    {
-                        var game = new Game
-                        {
-                            Name = nameNode.InnerText.Trim(),
-                            Price = priceNode.InnerText.Trim(),
-                        };
-
-                        games.Add(game);
-                    }
-                }
-            }
-
-            Games = games;
-        }
-
-
-
-        public event PropertyChangedEventHandler PropertyChanged; // Подія зміни властивостей
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) // Оновлення властивостей
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void TimesButton_Click(object sender, RoutedEventArgs e)
@@ -194,16 +162,18 @@ namespace KYRSOVA
             timerWindows.Show();
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-
-    public class Game // Клас ігор
+    public class Game
     {
-        public string Name { get; set; } // Назва гри
-        public string Price { get; set; } // Ціна гри
-
-
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string ImageUrl { get; set; }
     }
-
 }
--- RegistrationWindow.xaml.cs
+++ RegistrationWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
 using System.Text;
@@ -10,8 +9,7 @@ namespace KYRSOVA
 {
     public partial class RegistrationWindow : Window
     {
-        
-        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Registred;Integrated Security=True";
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Registred;Integrated Security=True";
 
         public RegistrationWindow()
         {
@@ -22,19 +20,18 @@ namespace KYRSOVA
         {
             try
             {
-                // Хешуємо пароль перед збереженням його в базі даних
                 string hashedPassword = HashPassword(password);
 
-                using (SqlConnection connection = new SqlConnection(connectionString)) // Підключаємося до бази даних
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     await connection.OpenAsync();
 
-                    string query = "INSERT INTO Regist (Username, Password) VALUES (@Username, @Password)"; // Запит на вставку даних
+                    string query = "INSERT INTO Regist (Username, Password) VALUES (@Username, @Password)";
 
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@Username", username);
-                        command.Parameters.AddWithValue("@Password", hashedPassword); // Зберігаємо хеш паролю
+                        command.Parameters.AddWithValue("@Password", hashedPassword);
 
                         await command.ExecuteNonQueryAsync();
                     }
@@ -42,43 +39,48 @@ namespace KYRSOVA
             }
             catch (SqlException ex)
             {
-                MessageBox.Show("Помилка при вставці даних: " + ex.Message);
+                ShowErrorMessage("Помилка при вставці даних: " + ex.Message);
             }
         }
 
         private string HashPassword(string password)
         {
-            // Використовуємо SHA256 для хешування паролю
             using (SHA256 sha256 = SHA256.Create())
             {
                 byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-
-                // Перетворюємо байти в рядок для збереження в базі даних
                 StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < hashedBytes.Length; i++)
+
+                foreach (byte b in hashedBytes)
                 {
-                    builder.Append(hashedBytes[i].ToString("x2"));
+                    builder.Append(b.ToString("x2"));
                 }
 
                 return builder.ToString();
             }
         }
 
-        private async void btnRegister_Click(object sender, RoutedEventArgs e)
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
         {
             string username = txtUsername.Text;
             string password = txtPassword.Password;
             if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
             {
-                MessageBox.Show("Введіть email і пароль!");
+                ShowErrorMessage("Введіть email і пароль!");
             }
             else
             {
-                await InsertUserAsync(username, password); // Викликаємо метод вставки даних
+                Task.Run(async () =>
+                {
+                    await InsertUserAsync(username, password);
+                }).Wait();
 
                 MessageBox.Show("Реєстрація успішна!");
             }
         }
 
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
